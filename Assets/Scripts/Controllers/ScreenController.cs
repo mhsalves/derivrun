0f200c6218ac2364743c0e3e9f649906895b99f9
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2017-04-03 21:14:03 -0400

    Criado Screen Controller.

---
+++ Assets/Scripts/Controllers/ScreenController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenController : MonoBehaviour {
+
+	void Start() {
+		DontDestroyOnLoad (gameObject);
+
+	}
+
+	public void AbrirGame() {
+		SceneManager.LoadScene (1);
+	}
+
+	public void AbrirEnd() {
+		SceneManager.LoadScene (2);
+	}
+
+	public void AbrirMain() {
+		SceneManager.LoadScene (0);
+	}
+
+}
