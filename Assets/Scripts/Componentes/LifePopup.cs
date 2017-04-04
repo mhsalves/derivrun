f570424ade13c711fcd8702e74b5f8cb487ee665
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2017-04-04 00:48:55 -0400

    Criado componente de seleção de Vidas.

---
+++ Assets/Scripts/Componentes/LifePopup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Componentes {
+
+	public class LifePopup : MonoBehaviour {
+
+		public List<LifePopupItem> listHearts;
+		private int quantidade = 1;
+
+		public int GetQuantidade() {
+			return quantidade;
+		}
+
+		void Start() {
+			AplicarQuantidade (1);
+
+		}
+
+		public ScreenController screenController;
+
+		public void AplicarQuantidade (int quantidade) {
+
+			this.quantidade = (quantidade < 1) ? 1 : (quantidade > 10) ? 10 : quantidade;
+
+			foreach (LifePopupItem lpi in listHearts) {
+				lpi.Apagar ();
+			}
+
+			for (int i = 0; i < this.quantidade; i++) {
+				this.listHearts[i].Acender();
+			}
+
+		}
+
+		public void ComeçarJogo() {
+			//TODO: Mandar a quantidade de vidas para o scene de GAME
+			screenController.AbrirGame ();
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/Componentes/LifePopupItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Componentes {
+
+	public class LifePopupItem : MonoBehaviour {
+		
+		private Animator animator;
+		private bool aceso = false;
+
+		void Start() {
+			this.animator = GetComponent<Animator> ();
+
+		}
+
+		public void Acender() {
+			this.aceso = true;
+			this.atualizar ();
+		}
+
+		public void Apagar(){
+			this.aceso = false;
+			this.atualizar ();
+		}
+
+		private void atualizar(){
+			this.animator.SetBool ("Aceso", this.aceso);
+		}
+
+	}
+
+}
