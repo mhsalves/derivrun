7fbf65408757f3002f426cc0a7af047bfb0485c3
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2017-04-18 19:58:12 -0400

    Colisão Lateral v0.2

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -29,14 +29,14 @@ public class GameController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		//this.lifeController = GameObject.Find ("LifeController").GetComponent<LifeController> ();
+		this.lifeController = GameObject.Find ("LifeController").GetComponent<LifeController> ();
 
 		var data = equationsResources.SelecionarDataAleatoria ();
 		equationsArea.Carregar (data);
 
 		tQuestion.text = "" + nQuestion;
 		tScore.text = "" + nScore;
-		//tLifes.text = "" + this.lifeController.GetVidas ();
+		tLifes.text = "" + this.lifeController.GetVidas ();
 
 		spawnInicialIsolado.IniciarInvocacao ();
 		contadorInicial.Comecar ();
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
+	
+	private float passo = 1f;
 
-	public float speed;
+	public MovementDirection movementDir = MovementDirection.FRENTE;
+	public bool podeMoverPraDireita = true;
+	public bool podeMoverPraEsquerda = true;
+	public bool podeDarPasso = true;
 
 	private static string animWalk = "Correr";
 	private static string animPower = "Poder";
 	private static string animJump = "Jump";
+
+	[System.Serializable]
+	public enum MovementDirection
+	{
+		DIREITA, ESQUERDA, FRENTE
+	}
 
 	private Animator animator;
 
@@ -20,7 +31,7 @@ public class PlayerBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 	public void LancarPoder() {
@@ -31,15 +42,47 @@ public class PlayerBehavior : MonoBehaviour {
 
 	}
 
-	public void PularProLado( bool toDir ) {
+	public void PassoDir() {
+		if (podeDarPasso) {
+			podeDarPasso = false;
+			this.movementDir = MovementDirection.DIREITA;
+			this.podeMoverPraEsquerda = true;
+			this.animator.SetTrigger (PlayerBehavior.animJump);
+		}
+	}
 
-		//TODO Mudar Posição pra direira ou esquerda
-		var newX = (toDir) ? Mathf.Abs (this.transform.localScale.x) : -Mathf.Abs (this.transform.localScale.x);
-		var toScale = new Vector3 ( newX, this.transform.localScale.y);
+	public void PassoEsq() {
+		if (podeDarPasso) {
+			podeDarPasso = false;
+			this.movementDir = MovementDirection.ESQUERDA;
+			this.podeMoverPraDireita = true;
+			this.animator.SetTrigger (PlayerBehavior.animJump);
+		}
+	}
 
-		this.transform.localScale = toScale;
-		this.animator.SetTrigger (PlayerBehavior.animJump);
+	public void LiberarPasso() {
+		podeDarPasso = true;
+	}
 
+	public void PularProLado( int fracao ) {
+
+		if (movementDir != MovementDirection.FRENTE) {
+			
+			var dir = (movementDir == MovementDirection.DIREITA) ? 1f : -1f;
+			var newSX = Mathf.Abs (this.transform.localScale.x) * dir;
+			var toScale = new Vector3 (newSX, this.transform.localScale.y);
+
+			print (movementDir);
+
+			if (
+				(movementDir == MovementDirection.DIREITA && podeMoverPraDireita) ||
+				(movementDir == MovementDirection.ESQUERDA && podeMoverPraEsquerda)) {
+
+				this.transform.Translate (passo / fracao * dir, 0f, 0f);
+				this.transform.localScale = toScale;
+			}
+
+		}
 	}
 
 	public void Andar() {
@@ -49,4 +92,23 @@ public class PlayerBehavior : MonoBehaviour {
 	public void Parar() {
 		this.animator.SetBool (PlayerBehavior.animWalk, false);
 	}
+
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		
+		if (other.tag == "LimitSide") {
+			
+			var lsb = other.GetComponent<LimitSideBehavior> ();
+
+			if (!lsb.toDireita) {
+				podeMoverPraDireita = false;
+			} else {
+				podeMoverPraEsquerda = false;
+			}
+
+		}
+
+	}
+
 }
