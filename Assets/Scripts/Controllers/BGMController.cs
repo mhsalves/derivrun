3c7a97fb19a6ee21588996637ce3efe91f254cef
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2017-04-26 00:21:42 -0400

    Aplicação de BGM

---
+++ Assets/Scripts/Controllers/BGMController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour {
+
+	private AudioSource source;
+
+	void Start() {
+		DontDestroyOnLoad (transform.gameObject);
+		source = GetComponent<AudioSource> ();
+
+	}
+
+}
