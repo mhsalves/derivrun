ea061269db7a08b5194a1f59472d1c8ec99070b6
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2017-05-08 21:36:15 -0400

    Pequenos ajustes de interface.

---
+++ Assets/Scripts/Componentes/SpacingLayout.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof (HorizontalOrVerticalLayoutGroup))]
+[RequireComponent(typeof (RectTransform))]
+[ExecuteInEditMode]
+public class SpacingLayout : MonoBehaviour {
+
+	[SerializeField] private int m_QuantidadeItens;
+	[Range(0, 1)][SerializeField] private float m_Percent = .1f;
+
+	// Use this for initialization
+	void OnRenderObject () {
+		var hovlg = GetComponent<HorizontalOrVerticalLayoutGroup> ();
+		var rt = GetComponent<RectTransform> ();
+
+		hovlg.spacing = rt.rect.width/m_QuantidadeItens * m_Percent;
+
+	}
+
+}
