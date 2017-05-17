eeb52fce187600cb06b5dc48eaaa5eaff75a7078
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2017-05-16 20:09:59 -0400

    Correção de bug pequeno.

---
+++ Assets/Scripts/Sprites/ArrowLaunchBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowLaunchBehaviour : MonoBehaviour {
+
+	[SerializeField] private ArrowLaunchBehaviour m_Twin;
+	[SerializeField] private float m_Forca;
+
+	private Collider2D m_OwnCollider;
+
+	void Awake () {
+		this.m_OwnCollider = GetComponent<Collider2D> ();
+
+	}
+
+	void OnTriggerEnter2D ( Collider2D other ) {
+
+		if (other.tag == "Player") {
+			this.AplicarEmpurrao ( other.gameObject );
+			this.m_Twin.Desabilitar ();
+			this.Desabilitar ();
+		}
+
+	}
+
+	private void AplicarEmpurrao( GameObject player ) {
+		var rgd2d = player.GetComponent<Rigidbody2D> ();
+		rgd2d.AddForce(new Vector2 (m_Forca, 0f));
+	}
+
+	public void Desabilitar() {
+		this.m_OwnCollider.enabled = false;
+	}
+
+}
