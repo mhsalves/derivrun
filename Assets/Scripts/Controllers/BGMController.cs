0b19705424d3def11f09905aa072787114150bbb
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2017-05-20 13:52:13 -0400

    Atualização do BGM CONTROLLER

--- Assets/Scripts/Controllers/BGMController.cs
+++ Assets/Scripts/Controllers/BGMController.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Controladores {
-
+	
+	[RequireComponent(typeof (AudioSource))]
 	public class BGMController : MonoBehaviour {
 
-	//	private AudioSource source;
+		private AudioSource m_AudioSource;
+
+		[Range(0f, 1f)] [SerializeField] private float m_VolumeInicial = 0.2f;
+
+		void Awake () {
+			this.m_AudioSource = GetComponent<AudioSource> ();
+
+		}
 
 		void Start() {
 			DontDestroyOnLoad (transform.gameObject);
-	//		source = GetComponent<AudioSource> ();
+
+			this.m_AudioSource.volume = m_VolumeInicial;
 
 		}
+
+		public void MudarVolume ( float volume ) {
+			this.m_AudioSource.volume = Mathf.Clamp(volume, 0f, 1f) ;
+		}
+
+		public void Mutar( bool mutar ) {
+			this.m_AudioSource.mute = mutar;
+		}
+
 
 	}
 
