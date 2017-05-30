0b7aba769eb2cc599b84f676171f64d3bb78dcc3
Matheus Henrique de Souza Alves <matheushalves@outlook.com.br>
2017-05-30 16:24:02 -0400

    Ajuste no Bloco Answer.

---
+++ Assets/Scripts/Sprites/SpriteText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Componentes {
+
+	public class SpriteText : MonoBehaviour {
+
+		void Start()
+		{
+			var parent = transform.parent;
+
+			var parentRenderer = parent.GetComponent<Renderer>();
+			var renderer = GetComponent<Renderer>();
+			renderer.sortingLayerID = parentRenderer.sortingLayerID;
+			renderer.sortingOrder = parentRenderer.sortingOrder;
+
+			var spriteTransform = parent.transform;
+			var pos = spriteTransform.position;
+
+		}
+	}
+
+}
